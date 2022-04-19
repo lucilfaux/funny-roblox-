6709b1b4092be4c2b211f0b781969db675725dc4
Banaanae <83927639+Banaanae@users.noreply.github.com>
2022-04-19 11:50:43 +1000

    changed to tween
    autofarm coming soon

--- build-a-boat.lua
+++ build-a-boat.lua
@@ -1,50 +1,7 @@
+getgenv().speed = 25 -- Lower = faster
+
+local TS = game:GetService("TweenService")
+local tween = TS:Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(speed), {CFrame = CFrame.new(-54, 65, 9485)})
 local plr = game.Players.LocalPlayer.Character.HumanoidRootPart
-
-
-function claim()
-    spawn(function()
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 1369.09009)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 2139.09009)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 2909.09009)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 3679.09009)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 4449.08984)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 5219.08984)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 5989.08984)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 6759.08984)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 7529.08984)
-        wait(0.2)
-
-        plr.CFrame = CFrame.new(-51.5656433, 65.0000458, 8299.08984)
-        wait(0.1)
-
-        plr.CFrame = CFrame.new(-53.9630127, -328.904144, 9418.69336)
-
-        wait(0.5)
-
-        plr.CFrame = CFrame.new(-54, -360, 9485)
-        wait(24)
-
-        workspace.ClaimRiverResultsGold:FireServer()
-    end)     
-   
-
-end
-
-
-claim()
+plr.CFrame = CFrame.new(-54, 65, -70)
+tween:Play()
